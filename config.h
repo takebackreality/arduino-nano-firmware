3fc59bb042f5dc40c78a78a43232ad4d880a3765
Mellad Sultani <140553611+takebackreality@users.noreply.github.com>
2025-08-29 12:59:10 -0700

    config.h

---
+++ config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Arduino Nano Pin Mapping
+#define PAN_PIN   9
+#define TILT_PIN  10
+#define FIRE_PIN  3
+
+// Servo Limits
+#define PAN_MIN   0
+#define PAN_MAX   180
+#define TILT_MIN  30
+#define TILT_MAX  150
+
+// Serial Config
+#define SERIAL_BAUD 115200
+
+#endif
