12c06d5a214582a7e463e1c3fd04afe31e2f974c
Mellad Sultani <140553611+takebackreality@users.noreply.github.com>
2025-08-29 13:09:55 -0700

    motor_control.cpp

---
+++ motor_control.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "config.h"
+
+namespace {
+    Servo panServo;
+    Servo tiltServo;
+}
+
+namespace MotorControl {
+    void init() {
+        panServo.attach(PAN_PIN);
+        tiltServo.attach(TILT_PIN);
+        panServo.write((PAN_MIN + PAN_MAX) / 2);
+        tiltServo.write((TILT_MIN + TILT_MAX) / 2);
+    }
+
+    void setPan(int angle) {
+        if (angle < PAN_MIN) angle = PAN_MIN;
+        if (angle > PAN_MAX) angle = PAN_MAX;
+        panServo.write(angle);
+    }
+
+    void setTilt(int angle) {
+        if (angle < TILT_MIN) angle = TILT_MIN;
+        if (angle > TILT_MAX) angle = TILT_MAX;
+        tiltServo.write(angle);
+    }
+}
